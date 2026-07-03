664cbc2768537b01986842ecbb6387ee215f6f2b
Aylin <aylin.ma24@gmail.com>
2026-07-03 23:42:11 +0200

    first commit

---
+++ src/main/java/Node.java
@@ -0,0 +1,13 @@
+public class Node {
+
+    int col;
+    double val;
+    Node next;
+
+    // constructor
+    public Node(int col, float val) {
+        this.col = col;
+        this.val = val;
+        next = null;
+    }
+}
---
+++ src/main/java/SparseMatrix.java
@@ -0,0 +1,4 @@
+public abstract class SparseMatrix {
+    public abstract double getPosition(int row, int col);
+    public abstract int getDimension();
+}
